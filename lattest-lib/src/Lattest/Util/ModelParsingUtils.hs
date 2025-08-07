bb31fcc6d5bb9d8d27f35845a56965f0a1b5bd4b
Maria Belen Rodriguez <mariabelen.rodriguez@utwente.nl>
2025-08-07 14:27:40 +0200

    doc: Add function docs

--- lattest-lib/src/Lattest/Util/ModelParsingUtils.hs
+++ lattest-lib/src/Lattest/Util/ModelParsingUtils.hs
@@ -11,6 +11,14 @@ import Data.Maybe (mapMaybe)
 import System.FilePath (replaceExtension)
 import Data.List (isSuffixOf)
 
+{-|
+    Read an .aut file representing an LTS from the provided filepath, parse its content and return:
+    - [String]: Input Alphabet
+    - [String]: Output Alphabet
+    - Set.Set String: Set of all LTS states
+    - Maybe [(String, IOAct String String, String)]: List of LTS transition tuples as (InitialState, Action, EndState)
+    NOTE: In order to parse actions correctly, inputs and outputs must end in _i and _o respectively. The first line of the .aut file is ignored.
+-}
 readAutFile :: FilePath -> IO ([String], [String], Set.Set String, Maybe [(String, IOAct String String, String)])
 readAutFile path = do
     contents <- TIO.readFile path
@@ -27,7 +35,7 @@ readAutFile path = do
     return (inputAlphabet, outputAlphabet, allStates, Just parsed)
 
 -- | Parse a text line of the form (state, action, state), and return the transition tuple
--- | NOTE: action labels are expected to end with either "_i" or "_o"
+-- | NOTE: only action labels finished in "_i" or "_o" are considered
 parseTupleLine :: T.Text -> Maybe (String, IOAct String String, String)
 parseTupleLine line =
     let stripped = T.strip line -- Remove trailing whitespaces
@@ -46,7 +54,7 @@ parseTupleLine line =
                    Nothing -- Non-valid action
         _ -> Nothing  -- Malformed line
 
--- Build dot file representation of LTS transitions
+-- | Build a .dot file representation of LTS transitions and save it in the specified File Path
 dumpLTSdot :: (Ord s, Show s, Ord i, Show i, Ord o, Show o) => FilePath -> [(s, IOAct i o, s)] -> IO ()
 dumpLTSdot path transitions = do
     let edges = [ (show from, T.unpack (T.replace "!" "" . T.replace "?" "" $ T.pack (show label)), show to)
