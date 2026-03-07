bc9edef8eadfcf7d6c28dc6f16218233c2f91462
Ramon Janssen <rjanssen@betterbe.com>
2026-03-07 22:52:07 +0100

    some more progress on the quickcheck tests

--- lattest-lib/test/Test/Lattest/Model/Symbolic/ValExpr/ValExpr.hs
+++ lattest-lib/test/Test/Lattest/Model/Symbolic/ValExpr/ValExpr.hs
@@ -7,17 +7,18 @@ module Test.Lattest.Model.Symbolic.ValExpr.ValExpr (
 where
 
 import Test.QuickCheck
+import Lattest.Model.Symbolic.ValExpr.FreeMonoidX as FM
 import Lattest.Model.Symbolic.ValExpr.ValExpr
 import Lattest.Model.Symbolic.ValExpr.ValExprDefs(Expr(Expr))
 import qualified GHC.Exts as GE
 import qualified Data.Set as Set
 import qualified Control.Monad as CM
 
-instance Arbitrary a => Arbitrary (Expr a) where
+instance Arbitrary (Expr a) where
     arbitrary = Expr <$> arbitrary
     shrink (view -> e) = Expr <$> shrink e
 
-instance Arbitrary a => Arbitrary (ExprView a) where
+instance Arbitrary (ExprView a) where
     arbitrary = sized genExpr
     shrink (Var _) = []
     shrink (Const c) = Const <$> shrink c
@@ -38,38 +39,71 @@ instance Arbitrary a => Arbitrary (ExprView a) where
 shrinkListExpr :: (t1 -> t2) -> t1 -> [t2]
 shrinkListExpr op es = fmap op (shrink es)
 
-genExpr :: Arbitrary t => Int -> Gen (ExprView t)
-genExpr 0 = oneof [
-    CM.liftM Var arbitrary,
-    CM.liftM Const arbitrary
-    ]
-genExpr n | n > 0 = oneof [
-    CM.liftM Var arbitrary,
-    CM.liftM Const arbitrary,
-    CM.liftM3 Ite subexpr2 subexpr2 subexpr2,
-    CM.liftM3 EqualInt subexpr2 subexpr2 subexpr2,
-    CM.liftM3 EqualBool subexpr2 subexpr2 subexpr2,
-    CM.liftM3 EqualString subexpr2 subexpr2 subexpr2,
-    CM.liftM2 Divide subexpr2 subexpr2,
-    CM.liftM2 Modulo subexpr2 subexpr2,
-    CM.liftM Sum (GE.fromList <$> listOf subexpr2Int),
-    CM.liftM Product (listOf subexpr2),
-    CM.liftM Length subexpr,
-    CM.liftM GezInt subexpr,
-    CM.liftM Not subexpr,
-    CM.liftM And (listOf subexpr2),
-    CM.liftM2 At subexpr2 subexpr2,
-    CM.liftM Concat (listOf subexpr2)
-    ]
-    where
-    subexpr :: Gen (ExprView t)
-    subexpr = genExpr (n - 1)
-    subexpr2 :: Gen (ExprView t)
-    subexpr2 = genExpr (n `div` 2)
-    subexpr2Int :: Gen (ExprView Integer)
-    subexpr2Int = subexpr2
+class ConcreteGenExpr t where
+    genExpr :: Int -> Gen (ExprView t)
 
-prop_symbolicEval :: Expr Integer -> Property
+instance ConcreteGenExpr Integer where
+    genExpr 0 = oneof [
+        CM.liftM Var arbitrary,
+        CM.liftM Const arbitrary
+        ]
+    genExpr n | n > 0 = oneof [
+        CM.liftM Var arbitrary,
+        CM.liftM Const arbitrary,
+        CM.liftM3 Ite subexpr2 subexpr2 subexpr2,
+        CM.liftM2 Divide subexpr2 subexpr2,
+        CM.liftM2 Modulo subexpr2 subexpr2,
+        CM.liftM Sum (FM.fromListT <$> listOf subexpr2),
+        CM.liftM Product (FM.fromListT <$> listOf subexpr2),
+        CM.liftM Length subexpr
+        ]
+        where
+        subexpr :: Gen (ExprView t)
+        subexpr = genExpr (n - 1)
+        subexpr2 :: Gen (ExprView t)
+        subexpr2 = genExpr (n `div` 2)
+
+instance ConcreteGenExpr Bool where
+    genExpr 0 = oneof [
+        CM.liftM Var arbitrary,
+        CM.liftM Const arbitrary
+        ]
+    genExpr n | n > 0 = oneof [
+        CM.liftM Var arbitrary,
+        CM.liftM Const arbitrary,
+        CM.liftM3 Ite subexpr2 subexpr2 subexpr2,
+        CM.liftM2 EqualInt subexpr2 subexpr2,
+        CM.liftM2 EqualBool subexpr2 subexpr2,
+        CM.liftM2 EqualString subexpr2 subexpr2,
+        CM.liftM GezInt subexpr,
+        CM.liftM Not subexpr,
+        CM.liftM And (Set.fromList <$> listOf subexpr2)
+        ]
+        where
+        subexpr :: Gen (ExprView t)
+        subexpr = genExpr (n - 1)
+        subexpr2 :: Gen (ExprView t)
+        subexpr2 = genExpr (n `div` 2)
+
+instance ConcreteGenExpr String where
+    genExpr 0 = oneof [
+        CM.liftM Var arbitrary,
+        CM.liftM Const arbitrary
+        ]
+    genExpr n | n > 0 = oneof [
+        CM.liftM Var arbitrary,
+        CM.liftM Const arbitrary,
+        CM.liftM3 Ite subexpr2 subexpr2 subexpr2,
+        CM.liftM2 At subexpr2 subexpr2,
+        CM.liftM Concat (listOf subexpr2)
+        ]
+        where
+        subexpr :: Gen (ExprView t)
+        subexpr = genExpr (n - 1)
+        subexpr2 :: Gen (ExprView t)
+        subexpr2 = genExpr (n `div` 2)
+
+prop_symbolicEval :: Expr Integer -> Bool
 prop_symbolicEval e = rightToMaybe (eval e) == concreteEval e
     where
     rightToMaybe (Left _) = Nothing
@@ -85,9 +119,9 @@ instance ConcreteEval Integer where
     concreteEval' (Ite i t e) = concreteIfThenElse i t e
     concreteEval' (Divide e1 e2) = concreteBinOp div e1 e2
     concreteEval' (Modulo e1 e2) = concreteBinOp mod e1 e2
-    concreteEval' (Length e) = concreteUnaryOp' length e
-    concreteEval' (Sum e1 e2) = concreteBinOp (+) e1 e2
-    concreteEval' (Product e1 e2) = concreteBinOp (*) e1 e2
+    concreteEval' (Length e) = concreteUnaryOp (Prelude.toInteger . length) e
+    concreteEval' (Sum es) = concreteBinOp (+) e1 e2
+    concreteEval' (Product es) = concreteBinOp (*) e1 e2
 
 instance ConcreteEval Bool where
     concreteEval' (Var v) = Nothing
@@ -96,8 +130,8 @@ instance ConcreteEval Bool where
     concreteEval' (EqualInt e1 e2) = concreteBinOp (==) e1 e2
     concreteEval' (EqualBool e1 e2) = concreteBinOp (==) e1 e2
     concreteEval' (EqualString e1 e2) = concreteBinOp (==) e1 e2
-    concreteEval' (GezInt e) = concreteUnaryOp' (>= 0) e
-    concreteEval' (Not e) = concreteUnaryOp' not e
+    concreteEval' (GezInt e) = concreteUnaryOp (>= 0) e
+    concreteEval' (Not e) = concreteUnaryOp not e
     concreteEval' (And e1 e2) = concreteBinOp (&&) e1 e2
 
 instance ConcreteEval String where
@@ -106,7 +140,7 @@ instance ConcreteEval String where
     concreteEval' (Ite i t e) = concreteIfThenElse i t e
     concreteEval' (At e1 e2) = concreteBinOp (flip drop) e1 e2
 
-concreteUnaryOp :: (ConcreteEval t1) => (t1 -> t2) -> ExprView t1 -> ExprView t2
+concreteUnaryOp :: (ConcreteEval t1) => (t1 -> t2) -> ExprView t1 -> Maybe t2
 concreteUnaryOp op e = do
     x <- concreteEval' e
     return $ op x
